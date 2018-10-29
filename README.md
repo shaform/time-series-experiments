31d04692f60182cd12574be8e6f1f76d2a7b289d
Shaform <shaform@gmail.com>
2018-10-29 18:25:24 -0400

    update scripts

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Time Series Experiments
 =======================
 
 Some simple experiments for time series analysis
+
+1. Execute `./scripts/parse_labeled.sh` to generate labled data.
--- scripts/parse_labeled.sh
+++ scripts/parse_labeled.sh
@@ -64,20 +64,3 @@ python -m ts.scripts.parse_mat \
 python -m ts.scripts.parse_mat \
   data/yahoo/yahoo-56.mat \
   parsed_data/labeled/yahoo-56.npy
-# python -m ts.scripts.parse_mat \
-#   data/yahoo/yahoo-7.mat \
-#   data/yahoo/yahoo-8.mat \
-#   data/yahoo/yahoo-16.mat \
-#   data/yahoo/yahoo-22.mat \
-#   data/yahoo/yahoo-27.mat \
-#   data/yahoo/yahoo-33.mat \
-#   data/yahoo/yahoo-37.mat \
-#   data/yahoo/yahoo-42.mat \
-#   data/yahoo/yahoo-45.mat \
-#   data/yahoo/yahoo-46.mat \
-#   data/yahoo/yahoo-50.mat \
-#   data/yahoo/yahoo-51.mat \
-#   data/yahoo/yahoo-54.mat \
-#   data/yahoo/yahoo-55.mat \
-#   data/yahoo/yahoo-56.mat \
-#   parsed_data/labeled/yahoo.npy
