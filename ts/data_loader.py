6126b3966115ef1cb2bee0d50a9a404c6c88592c
Shaform <shaform@gmail.com>
2018-10-31 01:01:01 +0000

    update data loader

--- ts/data_loader.py
+++ ts/data_loader.py
@@ -239,6 +239,7 @@ class LabeledDataLoader(object):
         self.batch_size = batch_size
         self.is_unlabelled = unlabelled
         self.full = full
+        self.num_variables = self.data_set.data.shape[1]
         self.device = device
 
     def unlabelled(self, full=False):
@@ -254,7 +255,7 @@ class LabeledDataLoader(object):
             full=full)
 
     def __len__(self):
-        if self.is_unlabelled and not -self.full:
+        if self.is_unlabelled and not self.full:
             return math.ceil((self.end - self.start) *
                              self.data_set.data.shape[1] / self.batch_size)
         else:
